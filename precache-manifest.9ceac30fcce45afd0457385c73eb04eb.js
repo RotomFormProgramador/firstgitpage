dcf9ec11d613b431af0c358c221cf6ca029f1f9f
RotomFormProgramador <59156337+RotomFormProgramador@users.noreply.github.com>
2020-03-04 22:01:31 -0600

    Updates

---
+++ precache-manifest.9ceac30fcce45afd0457385c73eb04eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a87af9bf10e479430a5c5018e03af0",
+    "url": "/firstgitpage/index.html"
+  },
+  {
+    "revision": "fa35362da3cfcbec9732",
+    "url": "/firstgitpage/static/css/main.ffa874b1.chunk.css"
+  },
+  {
+    "revision": "0afc997b8b3be2152983",
+    "url": "/firstgitpage/static/js/2.365bdcb9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/firstgitpage/static/js/2.365bdcb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa35362da3cfcbec9732",
+    "url": "/firstgitpage/static/js/main.fe2cf1d3.chunk.js"
+  },
+  {
+    "revision": "f1e7a444ba9e426a975c",
+    "url": "/firstgitpage/static/js/runtime-main.c4a22a68.js"
+  }
+]);
